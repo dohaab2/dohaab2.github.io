3376e2ce74823662d25acba2a95afaf3974a976b
Your Name <you@example.com>
2022-06-27 21:03:27 +0100

    init

---
+++ precache-manifest.f54f0d805c8a0329fe6e0aebcdb9c600.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "88e537c75fb99d51319c",
+    "url": "/static/css/main.8aee6b65.chunk.css"
+  },
+  {
+    "revision": "88e537c75fb99d51319c",
+    "url": "/static/js/main.88e537c7.chunk.js"
+  },
+  {
+    "revision": "6ab2da08eef6e976375c",
+    "url": "/static/js/1.6ab2da08.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f12e6e9c2c3cfbb6dc78bacdb9eadd25",
+    "url": "/index.html"
+  }
+];
